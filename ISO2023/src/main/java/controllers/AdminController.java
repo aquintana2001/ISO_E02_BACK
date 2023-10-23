33f607badb2ad33ec9edd2e2c593b9921a3ff29c
aquintana2001 <Alejandro.Quintana@alu.uclm.es>
2023-10-23 18:22:04 +0200

    Listar clientes y vehiculos y arreglos

--- ISO2023/src/main/java/controllers/AdminController.java
+++ ISO2023/src/main/java/controllers/AdminController.java
@@ -25,42 +25,17 @@ import org.springframework.web.bind.annotation.RestController;
 import entities.Vehiculo;
 import services.AdminService;
 import entities.User;
+import dao.AdminDAO;
 
 @RestController
-public class AdministradorController {
+public class AdminController {
 	@Autowired
 	private AdminService adminService;
-	
-	
-	
-	
-	@PostMapping("/register")
-	public void registrarse(@RequestBody Map<String, Object> info) {
-		String password1 = info.get("password1").toString();
-		String passwordd2 = info.get("password").toString();
-		if (!password1.equals(passwordd2))
-			throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Las contraseñas no coinciden");
-		
-		String nombre= info.get("nombre").toString();
-		String apellidos = info.get("apellidos").toString();
-		String email = info.get("email").toString();
-		String ciudad = info.get("ciudad").toString();
-		boolean carnet = Boolean.parseBoolean(info.get("carnet").toString());
-		String telefono = info.get("telefono").toString();
-		String dni = info.get("dni").toString();
-		
-		try {
-			this.adminService.registrarse(nombre, apellidos, email, password1, ciudad, carnet, telefono, dni);
-		}catch (Exception e) {
-			throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
-		}
-		
-	}
-	
+	private AdminDAO adminDAO;
 	@PutMapping("")
 	public Administrador updateAdmin(@PathVariable String email, @RequestBody Administrador admin) {
 		if(admin.pwdSecure(admin.getPassword())) {
-			Administrador administradorBBDD = adminRepositorio.findByEmail(email);
+			Administrador administradorBBDD = adminDAO.findByEmail(email);
 			administradorBBDD.setNombre(admin.getNombre());
 			administradorBBDD.setApellidos(admin.getApellidos());
 			administradorBBDD.setEmail(admin.getEmail());
@@ -74,14 +49,14 @@ public class AdministradorController {
 		}else {
 			throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Ha sucedido un error, no se cumple con los requisitos de nuestra politica de contraseñas");
 		}
-		this.adminService.actualizar(Nombre, Apellidos, Email, Password, Ciudad, Carnet, Telefono, Dni);
+		this.adminService.actualizar(nombre, Apellidos, Email, Password, Ciudad, Carnet, Telefono, Dni);
 	}
 	
 	
 	@DeleteMapping("")
 	public void deleteAdmin(@PathVariable String email) {
-		Administrador admin = adminRepositorio.findByEmail(email);
-		adminRepositorio.delete(admin);
+		Administrador admin = adminDAO.findByEmail(email);
+		adminDAO.delete(admin);
 	}
 	
 	
