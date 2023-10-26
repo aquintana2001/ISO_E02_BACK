bd34a9f7821cb09f85a27bda430a91093d50d1ee
aquintana2001 <Alejandro.Quintana@alu.uclm.es>
2023-10-26 18:56:04 +0200

    Ajustes seguridad

--- ISO2023/src/main/java/edu/uclm/esi/iso/ISO2023/dao/ClienteDAO.java
+++ ISO2023/src/main/java/edu/uclm/esi/iso/ISO2023/dao/ClienteDAO.java
@@ -2,10 +2,10 @@ package edu.uclm.esi.iso.ISO2023.dao;
 
 import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.core.query.Update;
 import edu.uclm.esi.iso.ISO2023.entities.Cliente;
 
 
 public interface ClienteDAO extends MongoRepository<Cliente,String>{
 	Optional<Cliente> findByEmail(String email);
+	Optional<Cliente> findByDni(String dni);
 }
