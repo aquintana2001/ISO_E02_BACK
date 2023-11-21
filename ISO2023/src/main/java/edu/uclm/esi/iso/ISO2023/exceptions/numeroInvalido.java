e24a4ab716bb178aca062159b0865c90bbcaad5f
Noel Fernández de Sevilla Sánchez <91826014+NoelFlajelo12@users.noreply.github.com>
2023-11-21 18:56:33 +0100

    Delete ISO2023/src/main/java/edu/uclm/esi/iso/ISO2023 directory

--- ISO2023/src/main/java/edu/uclm/esi/iso/ISO2023/exceptions/numeroInvalido.java
+++
@@ -1,9 +0,0 @@
-package edu.uclm.esi.iso.ISO2023.exceptions;
-
-@SuppressWarnings("serial")
-public class numeroInvalido extends Exception{
-	
-	public numeroInvalido(String error) {
-		super(error);
-	}
-}
