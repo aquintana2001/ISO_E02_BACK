62a6c5c39a8df17c0dddc6d42b77ac1df39a9f91
Noel Fernández de Sevilla Sánchez <91826014+NoelFlajelo12@users.noreply.github.com>
2023-11-21 18:58:00 +0100

    Add files via upload

---
+++ ISO2023/src/main/java/edu/uclm/esi/iso/ISO2023/exceptions/numeroInvalido.java
@@ -0,0 +1,9 @@
+package edu.uclm.esi.iso.ISO2023.exceptions;
+
+@SuppressWarnings("serial")
+public class numeroInvalido extends Exception{
+	
+	public numeroInvalido(String error) {
+		super(error);
+	}
+}
