ef10beab795de6f3a0aa0f2de79b844c0e4f7eb0
Noel Fernández de Sevilla Sánchez <91826014+NoelFlajelo12@users.noreply.github.com>
2023-11-22 11:04:28 +0100

    Delete ISO2023/src/main/java/edu/uclm/esi/iso/ISO2023 directory

--- ISO2023/src/main/java/edu/uclm/esi/iso/ISO2023/entities/Administrador.java
+++
@@ -1,9 +0,0 @@
-package edu.uclm.esi.iso.ISO2023.entities;
-
-
-public class Administrador extends User{
-	
-	public Administrador(String nombre, String apellidos, String email, String password, boolean activo, int intentos) {
-		super(nombre, apellidos, email, password, activo, intentos);
-	}
-}
